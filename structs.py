8bc7fd62bce8ad0a45c2631afa5241af38ae125c
askostin <askostin.mipt@yandex.ru>
2022-09-19 22:05:45 +0300

    fix(structs): correct Heap_min.sift_down()

--- structs.py
+++ structs.py
@@ -16,42 +16,66 @@ class Heap_min:
 		self.values = []
 		self.size = 0
 
+	def parent(self, i):
+		return (i-1)//2 if (i > 0) else None
+
+	def child_left(self, i):
+		return 2*i+1 if ((2*i+1) < self.size) else None
+
+	def child_right(self, i):
+		return 2*i+2 if ((2*i+2) < self.size) else None
+
 	def insert(self, x):
 		self.values.append(x)
 		self.size += 1
 		self.sift_up(self.size-1)
 
 	def sift_up(self, i):
-		if (i!=0):
-			j = (i-1)//2
-		while (i!=0) and (self.values[i] < self.values[j]):
+		if (i > 0):
+			j = self.parent(i)
+		while (i > 0) and (self.values[i] < self.values[j]):
 			self.values[i], self.values[j] = \
 				self.values[j], self.values[i]
 			i = j
-			j = (i-1)//2
+			j = self.parent(i)
 
 	def extract_min(self):
-		if not self.size:
+		if self.size == 0:
 			return None
 		tmp = self.values[0]
+		if self.size == 1:
+			self.values.pop()
+			return tmp
 		self.values[0] = self.values[-1]
 		self.values.pop()
-		if not self.size:
-			return None
 		self.size -= 1
 		self.sift_down(0)
 		return tmp
 
 	def sift_down(self, i):
-		while (2*i+1 <= self.size):
+		lc = self.child_left(i)
+		while lc:
+			rc = self.child_right(i)
 			j = i
-			if self.values[2*i+1] < self.values[i]:
-				j = 2*i+1
-			if (2*i+2 < self.size) and (self.values[2*i+2] < self.values[j]):
-				j = 2*i+2
+			more_than_left = (self.values[lc] < self.values[i])
+			if rc:
+				more_than_right = (self.values[rc] < self.values[i])
+				if more_than_left and more_than_right:
+					if (self.values[lc] <= self.values[rc]):
+						j = lc
+					else:
+						j = rc
+				elif more_than_left:
+					j = lc
+				elif more_than_right:
+					j = rc
+			elif more_than_left:
+				j = lc
 			if i==j:
 				break
 			self.values[i], self.values[j] = self.values[j], self.values[i]
+			i = j
+			lc = self.child_left(i)
 
 
 class Heap_max:
--- tests/test_structs.py
+++ tests/test_structs.py
@@ -7,16 +7,14 @@ a1 = Heap_min()
 for x in random.sample([1, 2, 3, 10], 4):
 	a1.insert(x)
 print(a1.values)
-print(a1.size)
 a1.extract_min()
 print(a1.values)
-print(a1.size)
+
+print('##########')
 
 a2 = Heap_min()
-for x in random.sample([10, 15, 30, 40, 40, 50, 100], 7):
+for x in [1, 2, 4, 5, 3, 4, 8]:
 	a2.insert(x)
 print(a2.values)
-print(a2.size)
 a2.extract_min()
 print(a2.values)
-print(a2.size)
