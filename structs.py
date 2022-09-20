6ed413129d85d04a2688ff98a6bf5e2e21f7ff49
Alex Kostin <askostin.mipt@yandex.ru>
2022-09-20 21:29:15 +0300

    test(structs): add test for @Heap_max and fix @Heap_max

--- structs.py
+++ structs.py
@@ -1,18 +1,18 @@
 # Heap implementation
 
-'''
-def Heap:
+def in_order(n1, n2, asc = True, strict = True):
+	""" Check if the order of two given numbers is correct. """
+	if strict:
+		return ((n1 < n2) if asc else (n1 > n2))
+	else:
+		return ((n1 <= n2) if asc else (n1 >= n2))
+
+class Heap:
 	types = {'min': 1, 'max': 0}
 	def __init__(self, type):
 		if type not in(list(types.keys())):
 			raise ValueError("Heap can be only 'min' or 'max'.")
 		self.type = type
-		self.values = []
-		self.size = 0
-'''
-
-class Heap_min:
-	def __init__(self):
 		self.values = []
 		self.size = 0
 
@@ -30,10 +30,11 @@ class Heap_min:
 		self.size += 1
 		self.sift_up(self.size-1)
 
-	def sift_up(self, i):
+	def sift_up(self, i, asc_order):
 		if (i > 0):
 			j = self.parent(i)
-		while (i > 0) and (self.values[i] < self.values[j]):
+		while (i > 0) and \
+			in_order(self.values[i], self.values[j], is_asc = asc_order):
 			self.values[i], self.values[j] = \
 				self.values[j], self.values[i]
 			i = j
@@ -57,9 +58,88 @@ class Heap_min:
 		while lc:
 			rc = self.child_right(i)
 			j = i
-			more_than_left = (self.values[lc] < self.values[i])
+			compare_lc = in_order(
+				self.values[lc],
+				self.values[i],
+				is_asc = asc_order
+				)
 			if rc:
-				more_than_right = (self.values[rc] < self.values[i])
+				compare_rc = in_order(
+					self.values[rc],
+					self.values[i],
+					is_asc = asc_order
+					)
+				if compare_lc and compare_rc:
+					if in_order(
+						self.values[lc],
+						self.values[rc],
+						is_asc = asc_order
+						):
+						j = lc
+					else:
+						j = rc
+				elif compare_lc:
+					j = lc
+				elif compare_rc:
+					j = rc
+			elif compare_lc:
+				j = lc
+			if i==j:
+				break
+			self.values[i], self.values[j] = self.values[j], self.values[i]
+			i = j
+			lc = self.child_left(i)
+
+
+class Heap_min:
+	def __init__(self):
+		self.values = []
+		self.size = 0
+
+	def parent(self, i):
+		return (i-1)//2 if (i > 0) else None
+
+	def child_left(self, i):
+		return 2*i+1 if ((2*i+1) < self.size) else None
+
+	def child_right(self, i):
+		return 2*i+2 if ((2*i+2) < self.size) else None
+
+	def insert(self, x):
+		self.values.append(x)
+		self.size += 1
+		self.sift_up(self.size-1)
+
+	def sift_up(self, i):
+		if (i > 0):
+			j = self.parent(i)
+		while (i > 0) and in_order(self.values[i], self.values[j]):
+			self.values[i], self.values[j] = \
+				self.values[j], self.values[i]
+			i = j
+			j = self.parent(i)
+
+	def extract_min(self):
+		if self.size == 0:
+			return None
+		tmp = self.values[0]
+		if self.size == 1:
+			self.values.pop()
+			return tmp
+		self.values[0] = self.values[-1]
+		self.values.pop()
+		self.size -= 1
+		self.sift_down(0)
+		return tmp
+
+	def sift_down(self, i):
+		lc = self.child_left(i)
+		while lc:
+			rc = self.child_right(i)
+			j = i
+			more_than_left = in_order(self.values[lc], self.values[i])
+			if rc:
+				more_than_right = in_order(self.values[rc], self.values[i])
 				if more_than_left and more_than_right:
 					if (self.values[lc] <= self.values[rc]):
 						j = lc
@@ -82,6 +162,15 @@ class Heap_max:
 	def __init__(self):
 		self.values = []
 		self.size = 0
+
+	def parent(self, i):
+		return (i-1)//2 if (i > 0) else None
+
+	def child_left(self, i):
+		return 2*i+1 if ((2*i+1) < self.size) else None
+
+	def child_right(self, i):
+		return 2*i+2 if ((2*i+2) < self.size) else None
 
 	def insert(self, x):
 		self.values.append(x)
@@ -110,12 +199,26 @@ class Heap_max:
 		return tmp
 
 	def sift_down(self, i):
-		while (2*i+1 <= self.size):
+		lc = self.child_left(i)
+		while lc:
+			rc = self.child_right(i)
 			j = i
-			if self.values[2*i+1] > self.values[i]:
-				j = 2*i+1
-			if (2*i+2 < self.size) and (self.values[2*i+2] > self.values[j]):
-				j = 2*i+2
+			less_than_left = (self.values[lc] > self.values[i])
+			if rc:
+				less_than_right = (self.values[rc] > self.values[i])
+				if less_than_left and less_than_right:
+					if (self.values[lc] <= self.values[rc]):
+						j = lc
+					else:
+						j = rc
+				elif less_than_left:
+					j = lc
+				elif less_than_right:
+					j = rc
+			elif less_than_left:
+				j = lc
 			if i==j:
 				break
 			self.values[i], self.values[j] = self.values[j], self.values[i]
+			i = j
+			lc = self.child_left(i)
--- tests/test_structs.py
+++ tests/test_structs.py
@@ -2,19 +2,40 @@ from algo.structs import *
 from algo.tests.test import general_test
 import random
 
-# testin Heap_min
-a1 = Heap_min()
-for x in random.sample([1, 2, 3, 10], 4):
-	a1.insert(x)
-print(a1.values)
-a1.extract_min()
-print(a1.values)
+print('\nTesting Heap_min:')
+h1 = Heap_min()
+a1 = random.sample([1, 2, 3, 10], 4)
+for x in a1:
+	h1.insert(x)
+print(h1.values)
+h1.extract_min()
+print(h1.values)
 
 print('##########')
 
-a2 = Heap_min()
-for x in [1, 2, 4, 5, 3, 4, 8]:
-	a2.insert(x)
-print(a2.values)
-a2.extract_min()
-print(a2.values)
+h2 = Heap_min()
+a2 = [1, 2, 4, 5, 3, 4, 8]
+for x in a2:
+	h2.insert(x)
+print(h2.values)
+h2.extract_min()
+print(h2.values)
+
+print('\nTesting Heap_max:')
+h3 = Heap_max()
+a3 = random.sample([1, 2, 3, 10], 4)
+for x in a3:
+	h3.insert(x)
+print(h3.values)
+h3.extract_max()
+print(h3.values)
+
+print('##########')
+
+h4 = Heap_max()
+a4 = [1, 2, 4, 5, 3, 4, 8]
+for x in a4:
+	h4.insert(x)
+print(h4.values)
+h4.extract_max()
+print(h4.values)
