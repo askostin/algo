ced3751179ea2da3b4bd0913c0c042e5c0bbb716
askostin <askostin.mipt@yandex.ru>
2023-01-08 21:47:31 +0300

    feat: add function permutations() to generate all permutations for given number

--- comb.py
+++ comb.py
@@ -28,3 +28,17 @@ def generate_permutations(N: int):
 			prefix.pop()
 
 	aux(N)
+
+# For given number @n generate list of all various numbers,
+# which are results of permutatiuons of digits of @n.
+def permutations(n: int):
+	s = str(n)
+	perms = []
+	def aux(lod: list, prefix = ""):
+		if not lod:
+			perms.append(prefix)
+		for i in range(len(lod)):
+			aux(lod[:i]+lod[i+1:], prefix + lod[i])
+
+	aux(s)
+	return list(set([int(x) for x in perms]))
